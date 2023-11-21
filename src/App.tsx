a656cc8c5e10ffd8e8cf600eb76a54ff7b3e581f
Aashish Paliwal <aashish.paliwal@live.com>
2023-11-21 18:44:10 +0530

    added basic structure and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const connectWallet = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <div className="card text-center my-5">
+        <div className="card-body">
+          <h1>Counter</h1>
+          <div className="my-5">
+            <h2 className="my-5">{count}</h2>
+            <button className="btn btn-success mx-3" 
+            onClick={() => setCount(count + 1)}
+            style={{ borderRadius: '50%', width: '120px', height: '120px', padding: '0' }}>
+              Increment Counter
+              </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
